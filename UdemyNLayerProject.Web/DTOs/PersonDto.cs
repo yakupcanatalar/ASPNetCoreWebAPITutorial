a1eb62d79a3466a5f9ec05980e4b49dd6c01e6e6
Yakup can Atalar <atalaryakupcan@gmail.com>
2022-01-02 21:55:39 +0300

    MVC application eklendi

---
+++ UdemyNLayerProject.Web/DTOs/PersonDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace UdemyNLayerProject.Web.DTOs
+{
+    public class PersonDto
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+    }
+}
